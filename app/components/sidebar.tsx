4b4caf12ad3e6e475b19fb7bdf23c25bd50c90ff
bestmikecat <62850225+bestmikecat@users.noreply.github.com>
2023-12-05 11:37:34 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          BestMike的智能助理
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          让我来解决任何事情!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
